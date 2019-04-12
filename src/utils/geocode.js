b43ec6796e58a9b075f1dfd2f6cd9b7902de6c4e
hbhatia <luckybhatia99@gmail.com>
2019-04-12 19:41:50 -0400

    second commit

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiaGJoYXRpYTciLCJhIjoiY2p1ZW53bm1kMDR0MDQzcDh2bXJjcWd6ZSJ9.R4XCn6wNCgZ64MlBpY-2Jw'
 
     request({ url, json: true }, (error, { body }) => {
-       // console.log(body);
+       
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
